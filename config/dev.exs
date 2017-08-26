a838e5b4a959d7caaf66fed2b6b3d91d95bd3596
mauricionr <mauricioskateboard@gmail.com>
2017-08-26 18:20:55 -0300

    testing

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :simple_auth, SimpleAuth.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "root",
+  password: "root",
   database: "simple_auth_dev",
   hostname: "localhost",
   pool_size: 10
